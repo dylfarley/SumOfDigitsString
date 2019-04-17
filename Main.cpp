a19cb8f8946fdf0543010863766456d1d3af9854
Dylan Farley <dylfarley@yahoo.com>
2019-04-17 11:45:15 -0700

    Prompt user for a number

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main(){
-
+	string numberStr;
+	
+	cout << "Enter a number: " << endl;
+	cin >> numberStr;
+	cout << "You entered: " << numberStr << endl;
+	
 
 	return 0;
 }
